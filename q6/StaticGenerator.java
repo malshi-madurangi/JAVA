24db34f8b71f5b17d531225814028a9c9f0f8b0b
malshi-madurangi <m.madurangidesilva@gmail.com>
2020-11-06 21:32:34 +0530

    Q6 is completed

---
+++ q6/StaticGenerator.java
@@ -0,0 +1,29 @@
+package q6;
+
+public class StaticGenerator {
+    static String findNextVersionNumber(String version, char type) {
+        String[] versionNum = version.split("\\.");
+        int[] intNum = new int[versionNum.length];
+        int i;
+        for (i = 0; i < versionNum.length; i++) {
+            String sNum = versionNum[i];
+            intNum[i] = Integer.parseInt(sNum);
+        }
+        switch (type) {
+            case 'i':
+                intNum[2] = ++intNum[2];
+                break;
+            case 'j':
+                intNum[1] = ++intNum[1];
+                intNum[2] = 00;
+                break;
+            case 'n':
+                intNum[0] = ++intNum[0];
+                intNum[2] = 0;
+                intNum[1] = 0;
+                break;
+        }
+        String newVersion = (intNum[0] + "." + intNum[1] + "." + intNum[2]);
+        return newVersion;
+    }
+}
