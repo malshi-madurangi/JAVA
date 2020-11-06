5a75dd34eb06d78424f15094001aa1702bcf1d3d
malshi-madurangi <m.madurangidesilva@gmail.com>
2020-11-06 22:36:51 +0530

    Modified Q5

--- q5/Test.java
+++ q5/Test.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.print("CamelCase text : ");
-        String str = obj.nextLine();
+        GetValues getValues = new GetValues();
+        String str = getValues.getInputs();
 
         StaticGenerator gen = new StaticGenerator();
         System.out.println("Sentence : " + gen.camelToString(str));
