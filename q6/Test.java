bafe5cd8cd3e663131f057c8e96461fb91843eeb
malshi-madurangi <m.madurangidesilva@gmail.com>
2020-11-06 22:47:04 +0530

    Modified Q6

--- q6/Test.java
+++ q6/Test.java
@@ -1,16 +1,11 @@
 package q6;
 
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        System.out.print("Current version : ");
-        String version = obj.nextLine();
-        System.out.print("Type of change (i for minor, j for major, n for non-backward compatible) : ");
-        char type = obj.next().charAt(0);
+        GetValues getValues = new GetValues();
+        String[] arr = getValues.getInputs();
 
         StaticGenerator gen = new StaticGenerator();
-        System.out.print("Next version : " + gen.findNextVersionNumber(version, type));
+        System.out.print("Next version : " + gen.findNextVersionNumber(arr[0], arr[1].charAt(0)));
     }
 }
