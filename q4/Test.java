59070c67705e2cd1acd2eb93a6df714b36398d0d
malshi-madurangi <m.madurangidesilva@gmail.com>
2020-11-07 09:46:38 +0530

    Q2, Q3, Q4, Q5, Q6 Updated

--- q4/Test.java
+++ q4/Test.java
@@ -1,16 +1,26 @@
 package q4;
+import q3.StaticsGenerator;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         runStaticGenerator();
-
     }
 
     private static void runStaticGenerator(){
-        int number = getInput();
-        StaticGenerator gen = new StaticGenerator();
-        displayOutput(gen.luckyNum(number));
+        Scanner prm = new Scanner(System.in);
+        int option;
+        do{
+            System.out.println("Do you want find lucky number? (Yes(enter 1) or No(enter 0) :");
+            option = prm.nextInt();
+            if(option == 1){
+                int number = getInput();
+                StaticGenerator gen = new StaticGenerator();
+                displayOutput(gen.luckyNum(number));
+            }else {
+                System.out.println("Thank you!!");
+            }
+        }while (option == 1);
     }
 
     private static int getInput(){
@@ -20,6 +30,6 @@ public class Test {
     }
 
     private static void displayOutput(int luckyNum){
-        System.out.print("Lucky number : " + luckyNum);
+        System.out.println("Lucky number : " + luckyNum);
     }
 }
--- q6/Test.java
+++ q6/Test.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         runStaticGenerator();
-
     }
 
     private static void runStaticGenerator(){
-        String[] arr = getInput();
-        StaticGenerator gen = new StaticGenerator();
-        displayOutput(gen.findNextVersionNumber(arr[0], arr[1].charAt(0)));
+        Scanner prm = new Scanner(System.in);
+        int option;
+        do{
+            System.out.println("Do you want to find next version number? (Yes(enter 1) or No(enter 0) :");
+            option = prm.nextInt();
+            if(option == 1){
+                String[] arr = getInput();
+                StaticGenerator gen = new StaticGenerator();
+                displayOutput(gen.findNextVersionNumber(arr[0], arr[1].charAt(0)));
+            }else {
+                System.out.println("Thank you!!");
+            }
+        }while (option == 1);
     }
 
     private static String[] getInput(){
@@ -26,6 +35,6 @@ public class Test {
     }
 
     private static void displayOutput(String str){
-        System.out.print("Next version : " + str);
+        System.out.println("Next version : " + str);
     }
 }
